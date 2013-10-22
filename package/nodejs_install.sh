4f7483dcd4762b29d893a5a18d0b49ea3e905f17
root <root@eip>
2013-10-22 18:09:11 +0200

    modification dash dans fonction Load_gif

--- package/nodejs_install.sh
+++ package/nodejs_install.sh
@@ -302,16 +302,16 @@ Load_gif ()
 
     while [ -d /proc/${pid} ];
     do
-        echo -ne "\r|"
+        printf "\r|"
         sleep 0.2
-        echo -ne "\r/"
+        printf "\r/"
         sleep 0.2
-        echo -ne "\r-"
+        printf "\r-"
         sleep 0.2
-        echo -ne "\r\\"
+        printf "\r\\"
         sleep 0.2
     done
-    echo -ne "\r ";
+    printf "\r";
 }
 
 
